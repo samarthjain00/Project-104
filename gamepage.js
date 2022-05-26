42ff22cf55a4a9d543bc109bd895e316bc7cde56
samarthjain00 <97680097+samarthjain00@users.noreply.github.com>
2022-05-26 18:47:08 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,25 @@
+
+var y =Math.floor(Math.random() * 10 + 1);
+
+var x = document.getElementById("guessField").value;
+
+if(x== y)
+{
+    alert("CONGRATUALATIONS!!!"+ playername+"YOU GUESSED IT CORRECT IT RIGHT IN"
+          +guess + "GUESS ");
+          guess = 1;
+}
+else if(x > y)
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+else
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A GREATER NUMBER");
+}
+    function playAgain(){
+        y = Math.floor(Math.random() * 10 + 1);
+    }
+
